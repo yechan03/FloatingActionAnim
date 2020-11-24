4473fe3b775ff03762bb810abaf4287008ce2b24
yechan0704 <key07040@naver.com>
2020-11-25 02:41:51 +0900

    은우야 버그 고쳐줬으니까 일어나

--- app/src/main/java/com/example/eunwoosong/MainActivity.java
+++ app/src/main/java/com/example/eunwoosong/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.eunwoosong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -16,13 +21,17 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private FloatingActionButton fab;
     private Animation animation;
+    private BottomNavigationView bottomNavigationView;
+    private BottomAppBar appBar;
+    private Boolean asd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //animation
+        bottomNavigationView = findViewById(R.id.bottomnav_main);
+        appBar = findViewById(R.id.bottombar_main);
         animation = AnimationUtils.loadAnimation(this, R.anim.anim);
 
         button = findViewById(R.id.button);
@@ -31,15 +40,20 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(v-> {
 
             if (fab.getVisibility()==View.GONE){
-
+                fab.show();
                 fab.setVisibility(View.VISIBLE);
-                fab.setAnimation(animation);
             }
             else{
+                fab.hide();
                 fab.setVisibility(View.GONE);
             }
 
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
